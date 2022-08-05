8a9416a2085065460e75fe17282d56461b26c4ba
Felipe Acosta <felipeacostacoronado@gmail.com>
2022-08-05 17:15:05 -0500

    Fin de semana

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+
+// Componente de la tabla y modulos
+import { TablaComponent } from './tabla/tabla.component';
+import { MaterialModule } from './material/material.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TablaComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
+    // Importacion de los modulos
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
