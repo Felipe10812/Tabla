749c96c7c2c9f6200a3dcd2653606c806dee5a7e
Felipe Acosta <felipeacostacoronado@gmail.com>
2022-08-10 17:12:26 -0500

    Todo por hoy

--- src/app/tabla/tabla.component.ts
+++ src/app/tabla/tabla.component.ts
@@ -1,7 +1,9 @@
-import { Component, AfterViewInit, ViewChild } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, OnInit } from '@angular/core';
 import { MatSort } from '@angular/material/sort';
 import { MatTable, MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
+import { TablaService } from './tabla.service';
+
 
 export interface PeriodicElement {
   name: string;
@@ -10,82 +12,45 @@ export interface PeriodicElement {
   symbol: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
-
-/* 
-  @title Data table with sorting, pagination, and filtering.
-*/
-
 @Component({
   selector: 'app-tabla',
   templateUrl: './tabla.component.html',
-  styles: [`
-    table {
-    width: 100%;
-  }
-
-  .mat-form-field {
-    font-size: 14px;
-    width: 100%;
-  }
-
-  td, th {
-    width: 25%;
-  } `]
+  styleUrls: ['./tabla.component.css']
 })
 
-export class TablaComponent {
+export class TablaComponent implements OnInit{
+
+  ELEMENT_DATA: PeriodicElement[] =[];
 
   // Nombre de las posibles columnas que se usaran 
-  columns = [
-    {
-      columnDef: 'position',
-      header: 'No.',
-      cell: (element: PeriodicElement) => `${element.position}`,
-    },
-    {
-      columnDef: 'name',
-      header: 'Nombre',
-      cell: (element: PeriodicElement) => `${element.name}`,
-    },
-    {
-      columnDef: 'weight',
-      header: 'Peso',
-      cell: (element: PeriodicElement) => `${element.weight}`,
-    },
-    {
-      columnDef: 'symbol',
-      header: 'Simbolo',
-      cell: (element: PeriodicElement) => `${element.symbol}`,
-    },
-    {
-      columnDef: 'Acciones',
-      header: 'Acciones',
-      cell: (element: PeriodicElement) => {""},
-    },
-  ];
-  dataSource = ELEMENT_DATA;
-  displayedColumns = this.columns.map(c => c.columnDef);
-  
-  
-  constructor(){}
-  
-  // @ViewChild(MatTable) table!: MatPaginator;
-  // @ViewChild(MatPaginator) paginator!: MatPaginator;
-  // @ViewChild(MatSort) sort!: MatSort;
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'acciones'];
+  dataSource!: MatTableDataSource<any>
 
-/* 
+  @ViewChild(MatTable) table!: MatPaginator;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor( private _tablaservice: TablaService ){
+
+  }
+
+  ngOnInit(): void {
+    this.cargarElementos();
+  }
+
+  // Carga los elementos desde el servicio
+  cargarElementos(){
+    this.ELEMENT_DATA = this._tablaservice.getElemento();
+    this.dataSource = new MatTableDataSource(this.ELEMENT_DATA);
+  }
+
+  // Siclo de vida de la paginacion 
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  // Filtro 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
@@ -93,7 +58,16 @@ export class TablaComponent {
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
     }
-  } */
+  }
+
+  // Eliminar 
+  eliminar( id: number ){
+    console.log(id)
+    // Se obtiene del servicio
+    this._tablaservice.eliminarDato(id);
+    this.cargarElementos();
+  }
 
   
+
 }
